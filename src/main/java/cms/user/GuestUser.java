36cb87ceeef6a3a3c813d83848e1b26980c06056
Gabriel Alves <gabrielfelipe.santos@ucsal.edu.br>
2025-08-20 21:00:13 -0300

    refactor: Remove getters, atributos e métodos duplicados.

--- src/main/java/cms/user/GuestUser.java
+++ src/main/java/cms/user/GuestUser.java
@@ -1,32 +1,9 @@
 package cms.user;
 
 public class GuestUser extends User {
-	private final int id;
-	private final String nome;
-	private final String email;
-	private final String password;
 
 	public GuestUser(int id, String nome, String email, String password) {
-		this.id = id;
-		this.nome = nome;
-		this.email = email;
-		this.password = password;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public boolean checkPassword(String p) {
-		return password.equals(p);
+        super(id, nome, email, password);
 	}
 
 	public void viewContent(int contentId) {
